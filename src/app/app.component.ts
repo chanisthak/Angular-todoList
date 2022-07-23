54b4fef42bb0639bb0ad799bc90ce4a7619dbefb
chanisthak <chanistha.k@gmail.com>
2022-07-23 15:52:09 +0700

    Todo List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './Todo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+  todos : Todo[]=JSON.parse(localStorage['myTodo']);
+  newTodo:string;
+  updateTodoName:string;
+  myTodo:any;
+
+  saveTodo(){
+    if(this.newTodo){
+      let todo = new Todo();
+      todo.name = this.newTodo;
+      todo.isCompleted = false;
+      todo.isEditing = false;
+      this.todos.push(todo);
+      window.localStorage.setItem('myTodo',JSON.stringify(this.todos) );
+      this.todos = JSON.parse(localStorage['myTodo']);
+      console.log(this.todos);
+      this.newTodo='';
+    }else {
+      alert('Please Enter Todo')
+    }
+  }
+
+  done(id:number){
+    this.todos[id].isCompleted = !this.todos[id].isCompleted;
+    window.localStorage.setItem('myTodo',JSON.stringify(this.todos) ); 
+  }
+
+  remove(id:number){
+    this.todos = this.todos.filter((v,i)=> i !== id);
+    window.localStorage.setItem('myTodo',JSON.stringify(this.todos) ); 
+  }
+  edit(id:number){
+    this.todos[id].isEditing = true;
+    this.updateTodoName = this.todos[id].name;
+    window.localStorage.setItem('myTodo',JSON.stringify(this.todos) ); 
+  }
+  updateTodo(id:number){
+    if(this.updateTodoName) {
+      this.todos[id].name = this.updateTodoName;
+      this.todos[id].isEditing = false;
+      window.localStorage.setItem('myTodo',JSON.stringify(this.todos) );
+      this.todos = JSON.parse(localStorage['myTodo']);
+    }
+     else {
+      alert('Edit can not be NULL')
+     }
+    }
+    cancelEdit(id:number){
+      this.todos[id].isEditing = false;
+      window.localStorage.setItem('myTodo',JSON.stringify(this.todos) ); 
+    }
 }
